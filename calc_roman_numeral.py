35134c96e34ae9bb521b29f2b3ee100c2b2cf0b5
J. Kristoffer Legind <klegind@gmail.com>
2025-11-25 09:01:51 +0100

    Update calc_roman_numeral.py

--- calc_roman_numeral.py
+++ calc_roman_numeral.py
@@ -7,13 +7,9 @@ def roman_To_Digits(romanNumeral):
     return_list = []
 
     roman_list = list(romanNumeral)
-    digi_list = [romnums[j] for j in roman_list]
+    digit_list = [romnums[j] for j in roman_list]
 
-    return digi_list
-
-
-# len_rom = len(roman_digits)
-# stop_value = len_rom
+    return digit_list
 
 
 def roman_calc(roman_digits):
@@ -22,21 +18,21 @@ def roman_calc(roman_digits):
     len_rom = len(roman_digits)
     r_list = []
     for i, j in enumerate(roman_digits):
-        print(f"i:{i} AND j:{j}")
-        print(f"The plus one:: value {roman_digits[int(i)]}, the position being:= {i}")
-        if i+1 == len_rom:
-            print('Returning ----- end of list reached')
+        # We enumerate to get an explicit index for the roman_digits list.
+        
+        if i+1 == len_rom: # Remember that the index starts at 0
+            # Returning ----- end of list reached
             r_list.append(j)
             return r_list
+        
         if j < roman_digits[i+1]:
             # Testing the rule that a numeral preceding a smaller numeral must be negated.
-            print("WE are in minus -!-!", 'we are appending', -j)
+            print("We are in minus -!-!", 'we are appending', -j)
             r_list.append(-j)
         else:
-            print('In regular ELSE condition')
             r_list.append(j)
 
-roman_input = 'xxiv'
+roman_input = 'mxxiv'
 roman_numbers = roman_To_Digits(roman_input)
 print(f"ORIGINAL numbers: {roman_numbers}")
 res = roman_calc(roman_numbers)
